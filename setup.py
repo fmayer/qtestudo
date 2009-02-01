effc720ca02ae8a9929e3e7c2a901f964fdda3c9
Florian Mayer <flormayer@aim.com>
2009-02-01 17:35:40 +0100

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# qtest - unittest UI using PyQt
+# Copyright (C) 2008 Florian Mayer
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import imp
+
+try:
+    # So that we can use 'develop'.
+    from setuptools import setup
+except:
+    # For end-users distutils is okay too.
+    from distutils.core import setup
+
+dep = []
+
+try:
+    imp.find_module('PyQt4')
+except ImportError:
+    dep.append('PyQt4')
+
+setup(
+    name='qtest',
+    version='0.1',
+    description='Graphical User Interface for the unittest framework.',
+    author='Florian Mayer',
+    author_email='flormayer@aim.com',
+    url='http://bitbucket.org/segfaulthunter/qtest-mainline/',
+    keywords='unittest gui ui user-interface',
+    license='GPL',
+    zip_safe=True,
+    py_modules=['qtest'],
+    install_requires=dep,
+)
