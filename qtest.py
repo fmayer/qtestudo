5edf23c06ccd4e7c22f10245a7765a50650f884a
Florian Mayer <flormayer@aim.com>
2009-02-02 13:28:32 +0100

    Extend docstring.

--- qtest.py
+++ qtest.py
@@ -45,6 +45,18 @@ class SomeTest(unittest.TestCase):
 
 if __name__ == '__main__':
     qtest.main()
+
+Additionally, if you run this file directly, you will be able to select
+the TestCases you want to run using File->Open, then open files and
+double click the TestCases you want to run, or press "Select All" to
+select all.
+
+Please note that if you open new TestCases after having opened others the
+same way before, only the new ones will be run.
+
+Test that appear italics in the list have output which can be viewed in the
+detailed view(double click). For tests with errors or failed tests the
+traceback is shown in the detailed view too.
 """
 
 
